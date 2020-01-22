280f7e9e3bb76df742ddd19b9df080242903626d
Andrew Chan <iiandrewchan@gmail.com>
2020-01-22 18:24:14 +0000

    add personal example usage

---
+++ example/jira_api.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	jira "jira-restapi-docker/api"
+)
+
+/*
+	In the jira app:
+
+	To find users on the system:
+	Setting > User management
+
+	To find the project key:
+	Project > Project Settings > Details
+
+	To get the custom field:
+	Setting > Issues > Custom Fields > Custom Field setting > Edit, the id in the url
+
+	To get the custom field options:
+	Setting > Issues > Custom Fields > Custom Field setting > Configure > Options > Edit Options
+*/
+
+func main() {
+
+	const host = "http://localhost:8080"
+	const issueurl = "/rest/api/2/issue"
+	const projectKey = "EX"
+	accptr := &jira.Account{"user", "user"}
+
+	si := jira.SimpleIssue{
+		URL:         host + issueurl,
+		ProjectKey:  projectKey,
+		Summary:     "Summary example",
+		Description: "Description example",
+		IssueType:   "Story",
+	}
+
+	jira.CreateIssue(si, accptr)
+
+	const id = "10208"
+	const CFOption = "priority"
+
+	icf := jira.IssueCF{
+		Issue: si,
+		CFID:  "customfield_" + id,
+		CF:    jira.CustomField{CFOption},
+	}
+
+	jira.CreateIssue(icf, accptr)
+
+	issueid := "10100"
+	iu := jira.IssueU{
+		URL: host + issueurl + "/" + issueid,
+		Fields: map[string]interface{}{
+			"summary":           "new summary",
+			"description":       "new description",
+			"customfield_10208": map[string]string{"value": "priority"},
+		},
+	}
+
+	iu.UpdateIssueStrict(accptr)
+
+}
